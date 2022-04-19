60cd312b754a81ba5965a54437120ccee64cc843
jayvish2000 <jayvish799@gmail.com>
2022-04-19 17:50:14 +0530

    update data

--- configuration/sendNotification.js
+++ configuration/sendNotification.js
@@ -1,34 +1,43 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
-var serviceAccount = require('./ana-ca1c7-firebase-adminsdk-cr3bz-d1052010c0.json');
+var serviceAccount = require("./adminbluemoon-firebase-adminsdk-k0690-4aace30d5f");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-exports.notification = async ({title, body}) => await admin.messaging().send({
-  data: { title, body },
-  notification: {},
-  android: {
-    priority: 'high',
-  },
-  topic: 'admin',
-  
-}).then(function(response) {
-  console.log("Successfully sent message:", response);
-}).catch(function(error) {
-  console.log("Error sending message:", error);
-});
+exports.notification = async ({ title, body }) =>
+  await admin
+    .messaging()
+    .send({
+      data: { title, body },
+      notification: {},
+      android: {
+        priority: "high",
+      },
+      topic: "admin",
+    })
+    .then(function (response) {
+      console.log("Successfully sent message:", response);
+    })
+    .catch(function (error) {
+      console.log("Error sending message:", error);
+    });
 
-exports.notificationTo = async ({title, body, token}) => await admin.messaging().send({
-  data: { title, body },
-  notification: {},
-  android: {
-    priority: 'high',
-  },
-  token: token
-}).then(function(response) {
-console.log("Successfully sent message:", response);
-}).catch(function(error) {
-console.log("Error sending message:", error);
-});
+exports.notificationTo = async ({ title, body, token }) =>
+  await admin
+    .messaging()
+    .send({
+      data: { title, body },
+      notification: {},
+      android: {
+        priority: "high",
+      },
+      token: token,
+    })
+    .then(function (response) {
+      console.log("Successfully sent message:", response);
+    })
+    .catch(function (error) {
+      console.log("Error sending message:", error);
+    });
